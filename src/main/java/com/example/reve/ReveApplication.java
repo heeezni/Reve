56cad783c9897610fb0093698cb6a24b4d262f58
heeezni <hjin3456@naver.com>
2025-07-26 08:50:04 +0900

    🎉 Start: 프로젝트 시작

---
+++ src/main/java/com/example/reve/ReveApplication.java
@@ -0,0 +1,13 @@
+package com.example.reve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reve/ReveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
